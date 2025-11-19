35c0bcf03d1a046b9ab3681414a5a0377d24c177
Jahid Hasan <jahid.softwindtech@gmail.com>
2025-11-19 18:21:19 +0600

    Qzone added

--- src/assets/asset.js
+++ src/assets/asset.js
@@ -2,10 +2,16 @@ import background from './bg.png';
 import cls from './class.png';
 import logo from './logo.png';
 import userLogo from './user.png';
+import classimg from './class.png';
+import playground from './playground.png';
+import swimming from './swimming.png';
 
 export const data = {
     background,
     cls,
     logo,
     userLogo,
+    classimg,
+    playground,
+    swimming,
 };
--- src/components/Home/QZone.jsx
+++ src/components/Home/QZone.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { data } from '../../assets/asset';
 
 function QZone() {
     return (
-        <div className='mt-5'>
-            <h2 className='font-bold'>QZone</h2>
+        <div className='mt-5 bg-gray-200'>
+            <h2 className='font-bold mb-5'>QZone</h2>
+            <div className='space-y-5 pl-20'>
+                <img
+                    src={data.swimming}
+                    alt=''
+                />
+                <img
+                    src={data.classimg}
+                    alt=''
+                />
+                <img
+                    src={data.playground}
+                    alt=''
+                />
+            </div>
         </div>
     );
 }
